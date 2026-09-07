dc8c911d22bfd3d1869705a6b9a0b942ceda5246
manish-dev3490 <kmanish87064@gmail.com>
2026-09-07 16:56:00 +0530

    designed a checkAuth api

--- src/routes/userAuth.js
+++ src/routes/userAuth.js
@@ -1,15 +1,43 @@
-const express = require('express');
+const express = require("express");
 const authRouter = express.Router();
-const {signUpHandler,logInHandler,logOutHandler,registerAdminHandler} =require('../controllers/userAuthController');
-const authValidation = require('../middlewares/auth');
-const adminRegisterValidation = require('../middlewares/admin');
+const {
+    signUpHandler,
+    logInHandler,
+    logOutHandler,
+    registerAdminHandler,
+    getProfile,
+} = require("../controllers/userAuthController");
+const authValidation = require("../middlewares/auth");
+const adminRegisterValidation = require("../middlewares/admin");
 
+authRouter.post("/signup", signUpHandler);
+authRouter.post("/login", logInHandler);
+authRouter.post("/logout", authValidation, logOutHandler);
+authRouter.post(
+    "/admin/register",
+    adminRegisterValidation,
+    registerAdminHandler,
+);
+authRouter.get("/profile", authValidation, getProfile);
+authRouter.get("/checkAuth", authValidation, (req, res) => {
+    try {
+        const user = req.user;
+        const reply = {
+            _id: user._id,
+            firstName: user.firstName,
+            emailId: user.emailId,
+            problemSolved: user.problemSolved
+        };
 
-authRouter.post('/signup', signUpHandler);
-authRouter.post('/login', logInHandler);
-authRouter.post('/logout',authValidation, logOutHandler);
-authRouter.post('/admin/register',adminRegisterValidation, registerAdminHandler);
-
-// authRouter.get('/user/profile', userProfileHandler);
+        res.status(200).json({
+            user: reply,
+            message: "you are already authenticated"
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error,
+        });
+    }
+});
 
 module.exports = authRouter;
